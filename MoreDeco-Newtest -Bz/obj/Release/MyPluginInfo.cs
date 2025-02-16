ac3fdcb2a2e718d8e9fdde4436994be2b3be5cef
M3dicCookie <pred102@hotmail.com>
2025-02-16 09:36:26 -0800

    Delete MoreDeco-Newtest -Bz directory

--- MoreDeco-Newtest -Bz/obj/Release/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CustomItems
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "CustomItems";
-        public const string PLUGIN_NAME = "";
-        public const string PLUGIN_VERSION = "1.0.0.0";
-    }
-}
