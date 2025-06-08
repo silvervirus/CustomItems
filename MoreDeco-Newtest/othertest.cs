9e5be9f0052555446e950d45d28365e3f1694aa1
M3dicCookie <pred102@hotmail.com>
2025-06-07 23:00:17 -0700

    Delete MoreDeco-Newtest directory

--- MoreDeco-Newtest/othertest.cs
+++
@@ -1,7 +0,0 @@
-namespace System.Runtime.CompilerServices;
-
-public class RequiredMemberAttribute : Attribute { }
-public class CompilerFeatureRequiredAttribute : Attribute
-{
-    public CompilerFeatureRequiredAttribute(string name) { }
-}
--- MoreDeco-Newtest/test.cs
+++
@@ -1,7 +0,0 @@
-namespace System.Diagnostics.CodeAnalysis
-{
-    [AttributeUsage(AttributeTargets.Constructor, Inherited = false)]
-    internal sealed class SetsRequiredMembersAttribute : Attribute
-    {
-    }
-}
